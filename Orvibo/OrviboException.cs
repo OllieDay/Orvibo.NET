0bd84a45e7849c47f92d7331b6849d2b1e68336a
Ollie Day <OllieDay@users.noreply.github.com>
2015-11-23 17:43:21 +0000

    Add exception classes

---
+++ Orvibo/OrviboException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Orvibo
+{
+    /// <summary>
+    ///     Represents Orvibo errors that occur during application execution.
+    /// </summary>
+    public class OrviboException : Exception
+    {
+        /// <summary>
+        ///     Initializes a new instance of the OrviboException class.
+        /// </summary>
+        internal OrviboException() {}
+
+        /// <summary>
+        ///     Initializes a new instance of the OrviboException class with a specified error message.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        internal OrviboException(string message) : base(message) {}
+
+        /// <summary>
+        ///     Initializes a new instance of the OrviboException class with a specified error message and a reference to the inner
+        ///     exception that is the cause of this exception.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        /// <param name="innerException">
+        ///     The exception that is the cause of the current exception, or a null reference (Nothing in Visual Basic) if no inner
+        ///     exception is specified.
+        /// </param>
+        internal OrviboException(string message, Exception innerException) : base(message, innerException) {}
+    }
+}
