5b6f04dbe6f1293e604305d7ab55d4a99e77a8f2
Ollie Day <OllieDay@users.noreply.github.com>
2015-12-02 17:19:36 +0000

    Add ISocket interface for controlling socket

---
+++ Orvibo/ISocket.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace Orvibo
+{
+    public interface ISocket
+    {
+        event EventHandler<EventArgs> StateChanged;
+
+        event EventHandler<EventArgs> Subscribed;
+
+        event EventHandler<EventArgs> Unsubscribed;
+
+        IPAddress IPAddress { get; }
+
+        bool IsSubscribed { get; }
+
+        PhysicalAddress MacAddress { get; }
+
+        SocketState State { get; }
+
+        void Off();
+
+        void On();
+
+        void Subscribe();
+
+        void Unsubscribe();
+    }
+}
