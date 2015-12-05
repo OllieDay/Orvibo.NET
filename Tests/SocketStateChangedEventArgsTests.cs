7916fb43edbf73b18e1393092398dad535a754cc
Ollie Day <OllieDay@users.noreply.github.com>
2015-12-05 10:57:59 +0000

    Add tests for SocketStateChangedEventArgs

---
+++ Tests/SocketStateChangedEventArgsTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Orvibo;
+
+namespace Tests
+{
+    [TestClass]
+    public sealed class SocketStateChangedEventArgsTests
+    {
+        [TestMethod]
+        public void CreateNewSocketStateChangedEventArgs_FromState_ShouldBeValid()
+        {
+            var e = new SocketStateChangedEventArgs(SocketState.Off, SocketState.On);
+
+            Assert.AreEqual(e.FromState, SocketState.Off);
+        }
+
+        [TestMethod]
+        public void CreateNewSocketStateChangedEventArgs_ToState_ShouldBeValid()
+        {
+            var e = new SocketStateChangedEventArgs(SocketState.Off, SocketState.On);
+
+            Assert.AreEqual(e.ToState, SocketState.On);
+        }
+    }
+}
