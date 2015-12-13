4ad5ffd5de0b5721569bb39aaa0ceb357d3ce5b3
Ollie Day <OllieDay@users.noreply.github.com>
2015-12-13 14:09:59 +0000

    Make SocketFactory class static

--- Orvibo/SocketFactory.cs
+++ Orvibo/SocketFactory.cs
@@ -11,50 +11,32 @@ namespace Orvibo
     /// <summary>
     ///     Provides functionality for creating sockets.
     /// </summary>
-    public class SocketFactory : IDisposable
+    public static class SocketFactory
     {
         /// <summary>
         ///     Message receiver.
         /// </summary>
-        private readonly IMessageReceiver _messageReceiver;
+        private static readonly IMessageReceiver MessageReceiver = new MessageReceiver(new MessageParser());
 
         /// <summary>
         ///     Message sender.
         /// </summary>
-        private readonly IMessageSender _messageSender;
+        private static readonly IMessageSender MessageSender = new MessageSender();
 
         /// <summary>
         ///     List of sockets registered to receive messages.
         /// </summary>
-        private readonly List<Socket> _sockets = new List<Socket>();
-
-        /// <summary>
-        ///     Creates a new instance of the SocketFactory class.
-        /// </summary>
-        public SocketFactory() : this(new MessageReceiver(new MessageParser()), new MessageSender()) {}
-
-        /// <summary>
-        ///     Creates a new instance of the SocketFactory class.
-        /// </summary>
-        /// <param name="messageReceiver">Message receiver.</param>
-        /// <param name="messageSender">Message sender.</param>
-        internal SocketFactory(IMessageReceiver messageReceiver, IMessageSender messageSender)
-        {
-            _messageReceiver = messageReceiver;
-            _messageSender = messageSender;
-
-            _messageReceiver.MessageReceived += MessageReceived;
-        }
+        private static readonly List<Socket> Sockets = new List<Socket>();
 
         /// <summary>
         ///     Creates a new socket with the specified MAC address and IP address.
         /// </summary>
         /// <param name="macAddress">MAC address.</param>
         /// <param name="ipAddress">IP address.</param>
-        /// <returns></returns>
-        public ISocket Create(PhysicalAddress macAddress, IPAddress ipAddress)
+        /// <returns>Created socket.</returns>
+        public static ISocket Create(PhysicalAddress macAddress, IPAddress ipAddress)
         {
-            var socket = new Socket(macAddress, ipAddress, new UnicastMessageSender(_messageSender));
+            var socket = new Socket(macAddress, ipAddress, new UnicastMessageSender(MessageSender));
 
             socket.Subscribing += SocketSubscribing;
             socket.Unsubscribing += SocketUnsubscribing;
@@ -63,22 +45,13 @@ namespace Orvibo
         }
 
         /// <summary>
-        ///     Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
-        /// </summary>
-        public void Dispose()
-        {
-            _messageReceiver.Dispose();
-            _messageSender.Dispose();
-        }
-
-        /// <summary>
         ///     IMessageReceiver.MessageReceived event callback.
         /// </summary>
         /// <param name="sender">Sender.</param>
         /// <param name="e">Event args.</param>
-        private void MessageReceived(object sender, MessageReceivedEventArgs e)
+        private static void MessageReceived(object sender, MessageReceivedEventArgs e)
         {
-            _sockets.FirstOrDefault(s => s.MacAddress.Equals(e.Message.MacAddress))?.Process(e.Message);
+            Sockets.FirstOrDefault(s => s.MacAddress.Equals(e.Message.MacAddress))?.Process(e.Message);
         }
 
         /// <summary>
@@ -86,14 +59,14 @@ namespace Orvibo
         /// </summary>
         /// <param name="sender">Sender.</param>
         /// <param name="e">Event args.</param>
-        private void SocketSubscribing(object sender, EventArgs e)
+        private static void SocketSubscribing(object sender, EventArgs e)
         {
-            if (!_sockets.Any())
+            if (!Sockets.Any())
             {
-                _messageReceiver.Start();
+                MessageReceiver.Start();
             }
 
-            _sockets.Add((Socket) sender);
+            Sockets.Add((Socket) sender);
         }
 
         /// <summary>
@@ -101,13 +74,13 @@ namespace Orvibo
         /// </summary>
         /// <param name="sender">Sender.</param>
         /// <param name="e">Event args.</param>
-        private void SocketUnsubscribing(object sender, EventArgs e)
+        private static void SocketUnsubscribing(object sender, EventArgs e)
         {
-            _sockets.Remove((Socket) sender);
+            Sockets.Remove((Socket) sender);
 
-            if (!_sockets.Any())
+            if (!Sockets.Any())
             {
-                _messageReceiver.Stop();
+                MessageReceiver.Stop();
             }
         }
     }
