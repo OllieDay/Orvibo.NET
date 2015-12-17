f5d4552d938145a63f8531ab75aa7c18a3e297ea
Ollie Day <OllieDay@users.noreply.github.com>
2015-12-17 20:26:35 +0000

    Add OutboundKeepaliveMessage class

---
+++ Orvibo/Messaging/Outbound/OutboundKeepaliveMessage.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.NetworkInformation;
+
+namespace Orvibo.Messaging.Outbound
+{
+    /// <summary>
+    ///     Sent to a socket to maintain subscription. If 5 minutes elapses with no message sent to a socket then the
+    ///     subscription is cancelled.
+    /// </summary>
+    internal sealed class OutboundKeepaliveMessage : OutboundAddressableMessage
+    {
+        /// <summary>
+        ///     Message data.
+        /// </summary>
+        private static readonly byte[] Data = { 0x00, 0x00, 0x00, 0x00 };
+
+        /// <summary>
+        ///     Initializes a new instance of the OutboundKeepaliveMessage class.
+        /// </summary>
+        /// <param name="macAddress">Socket MAC address.</param>
+        public OutboundKeepaliveMessage(PhysicalAddress macAddress) : base(macAddress) {}
+
+        /// <summary>
+        ///     Gets the command for the message type.
+        /// </summary>
+        protected override byte[] Command { get; } = { 0x68, 0x62 };
+
+        /// <summary>
+        ///     Gets the data for the message type.
+        /// </summary>
+        /// <returns>Data for the messae type.</returns>
+        protected override byte[] GetData()
+        {
+            return Combine(base.GetData(), Data);
+        }
+    }
+}
---
+++ Tests/OutboundKeepaliveMessageTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.NetworkInformation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Orvibo.Messaging.Outbound;
+
+namespace Tests
+{
+    [TestClass]
+    public sealed class OutboundKeepaliveMessageTests
+    {
+        [TestMethod]
+        public void GetPayload_ShouldBeValid()
+        {
+            var macAddress = new PhysicalAddress(new byte[] { 0, 0, 0, 0, 0, 0 });
+            var message = new OutboundKeepaliveMessage(macAddress);
+
+            var expected = new byte[] { 0x68, 0x64, 0x00, 0x16, 0x68, 0x62, 0, 0, 0, 0, 0, 0, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0, 0, 0, 0 };
+            var actual = message.GetPayload();
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
